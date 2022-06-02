d6207d7ca709d75c185f099ac57fc2c5b693ae23
Furkan Kadıoğlu <iam@frkn.com.tr>
2022-06-03 01:21:02 +0300

    getFutureEventsWithWorkshops (/futureevents) fix

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -183,6 +183,8 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        return Event::with('workshops')->whereHas('workshops', function($query) {
+            $query->where('start', '>=', Date::now()->format('Y-m-d H:i:s'));
+        })->get();
     }
 }
