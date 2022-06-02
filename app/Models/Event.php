e0f69c388cbad97ce7562ec6696ff279a2ae7773
Furkan Kadıoğlu <iam@frkn.com.tr>
2022-06-03 00:46:01 +0300

    Workshop one to many relation added

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * An event may have multiple workshops
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function workshops()
+    {
+        return $this->hasMany(Workshop::class, 'event_id', 'id');
+    }
 }
